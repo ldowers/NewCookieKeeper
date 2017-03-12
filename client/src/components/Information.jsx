0d843fe4e05ec7bef06b4b23346fdffff8118abd
Lori Dowers <ldowers@cfl.rr.com>
2017-03-11 19:44:29 -0500

    Added react-bootstrap-table for girls info

--- client/src/components/Information.jsx
+++ client/src/components/Information.jsx
@@ -1,18 +1,68 @@
 import React, { PropTypes } from 'react';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
+//======================================
+//code to make cells editable
+const cellEditProp = {
+    mode: 'click'
+};
+//======================================
+
+//======================================
+//code to insert rows
+function onAfterInsertRow(row) {
+    let newRowStr = '';
+
+    for (const prop in row) {
+        newRowStr += prop + ': ' + row[prop] + ' \n';
+    }
+    alert('The new row is:\n ' + newRowStr);
+}
+
+const options = {
+    afterInsertRow: onAfterInsertRow   // A hook for after insert rows
+};
+//======================================
+
+//======================================
+//code to delete rows
+function onAfterDeleteRow(rowKeys) {
+    alert('The rowkey you drop: ' + rowKeys);
+}
+
+const deleteOptions = {
+    afterDeleteRow: onAfterDeleteRow  // A hook for after droping rows.
+};
+
+// If you want to enable deleteRow, you must enable row selection also.
+const selectRowProp = {
+    mode: 'checkbox'
+};
+//======================================
 
 const Information = ({girls}) => (
   <div>
-  <Card className="container">
-    <CardTitle
-      title="Girls info"
-    />
-    <div>
-      This is where girls info goes
-    </div>
-    {/*} {girls && <CardText style={{ fontSize: '12px', color: 'blue' }}>{girls[0]}</CardText>}*/}
-  </Card>
+    <Card className="container">
+      <CardTitle
+        title="Girls info"
+      />
+    </Card>
+    
+    {/*Girls Info Table*/}
+    <BootstrapTable 
+    data={girls} 
+    cellEdit={cellEditProp} 
+    insertRow={true} 
+    deleteRow={true} 
+    selectRow={selectRowProp} 
+    exportCSV={ true }
+    options={options} 
+    options={deleteOptions}
+    csvFileName='Girls Info.csv'>
+      <TableHeaderColumn dataField='id' csvHeader='ID' isKey={true}>ID#</TableHeaderColumn>
+      <TableHeaderColumn dataField='name' csvHeader="Girl's Name">Girl's Name</TableHeaderColumn>
+    </BootstrapTable>
   </div>
 );
 
