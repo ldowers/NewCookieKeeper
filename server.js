9dfd96d68ccefa7bbaa2295c8161df80e0f5a905
Lori Dowers <ldowers@cfl.rr.com>
2017-03-16 06:27:50 -0400

    Added body data parsing for JSON and text

--- server.js
+++ server.js
@@ -22,8 +22,13 @@ const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
-// tell the app to parse HTTP body messages
+
+// set up the Express app to handle data parsing
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 // pass the passport middleware
 app.use(passport.initialize());
 
