4d45b80fb936ed1e20c718a4cbe8d6489ccbcf01
Amanda Wang <amandawang@Amandas-MacBook-Pro.local>
2017-03-17 18:34:45 -0400

    removed generic dashboard messag

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,14 +3,17 @@ import { Card, CardTitle, CardText } from 'material-ui/Card';
 
 
 const Dashboard = ({ secretData }) => (
-  <Card className="container">
-    <CardTitle
-      title="Dashboard"
-      subtitle="You should get access to this page only after authentication."
-    />
 
-    {secretData && <CardText style={{ fontSize: '16px', color: 'green' }}>{secretData}</CardText>}
-  </Card>
+  <div>
+    </div>
+  //<Card className="container">
+ //   <CardTitle
+ //     title="Dashboard"
+  //    subtitle="You should get access to this page only after authentication."
+  //  />
+
+  //  {secretData && <CardText style={{ fontSize: '16px', color: 'green' }}>{secretData}</CardText>}
+ // </Card>
 );
 
 Dashboard.propTypes = {
