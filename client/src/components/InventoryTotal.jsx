24194dc0798755da1954860194432da16583d071
Lori Dowers <ldowers@cfl.rr.com>
2017-03-17 23:22:23 -0400

    Updated Inventory Page to get real data and calculate totals

--- client/src/components/InventoryTotal.jsx
+++ client/src/components/InventoryTotal.jsx
@@ -2,82 +2,6 @@ import React, { PropTypes } from 'react';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
-//======================================
-//code to make cells editable
-const cellEditProp = {
-    mode: 'click'
-};
-//======================================
-
-//======================================
-//code to insert rows
-function onAfterInsertRow(row) {
-    let newRowStr = '';
-
-    for (const prop in row) {
-        newRowStr += prop + ': ' + row[prop] + ' \n';
-    }
-    alert('The new row is:\n ' + newRowStr);
-}
-
-const options = {
-    afterInsertRow: onAfterInsertRow   // A hook for after insert rows
-};
-//======================================
-
-//======================================
-//code to delete rows
-function onAfterDeleteRow(rowKeys) {
-    alert('The rowkey you drop: ' + rowKeys);
-}
-
-const deleteOptions = {
-    afterDeleteRow: onAfterDeleteRow  // A hook for after droping rows.
-};
-
-// If you want to enable deleteRow, you must enable row selection also.
-const selectRowProp = {
-    mode: 'checkbox'
-};
-//======================================
-
-function addUpCookies(inventoryTotal) {
-    if(inventoryTotal.length === 0){
-        return
-    }
-    console.log("inventory total: " + JSON.stringify(inventoryTotal, null, 4));
-
-    for(let i = 0; i < inventoryTotal.length; i++){
-        inventoryTotal[i].total =  
-        inventoryTotal[i].TAL +
-        inventoryTotal[i].SMR + 
-        inventoryTotal[i].LEM + 
-        inventoryTotal[i].SB + 
-        inventoryTotal[i].TM + 
-        inventoryTotal[i].PBP + 
-        inventoryTotal[i].CD + 
-        inventoryTotal[i].PBS + 
-        inventoryTotal[i].GFT + 
-        inventoryTotal[i].MCS; 
-    }
-
-    return inventoryTotal;
-}
-
-// function calculateRemaining (inventoryTotal){
-//     if (inventoryTotal.length ===0){
-//         return
-//     }
-
-//     for (let i=0; i<inventoryTotal.length; i++){
-//         inventoryTotal[i].remaining =
-//         inventoryTotal[i].TAL -
-//         inventoryTotal[i+1].TAL -
-//         inventoryTotal[i+2]; 
-//     }
-// }
-
-
 const InventoryTotal = ({inventoryTotal}) => (
   <div>
     <Card className="container">
@@ -89,16 +13,10 @@ const InventoryTotal = ({inventoryTotal}) => (
     {/*Inventory Totals Table*/}
     <BootstrapTable 
     data={inventoryTotal} 
-    cellEdit={cellEditProp} 
-    insertRow={true} 
-    deleteRow={true} 
-    selectRow={selectRowProp} 
     exportCSV={ true }
-    options={options} 
-    options={deleteOptions}
     csvFileName='Inventory Totals.csv'>
-      
-       <TableHeaderColumn dataField='_id' csvHeader="ID#" isKey={true} hidden>ID #</TableHeaderColumn>
+
+      <TableHeaderColumn dataField='_id' csvHeader="ID#" isKey={true} hidden>ID #</TableHeaderColumn>
       <TableHeaderColumn dataField='from' csvHeader="From">From</TableHeaderColumn>
       <TableHeaderColumn dataField='TAL' csvHeader="TAL" >TAL</TableHeaderColumn>
       <TableHeaderColumn dataField='SMR' csvHeader="SMR">SMR</TableHeaderColumn>
@@ -110,7 +28,8 @@ const InventoryTotal = ({inventoryTotal}) => (
       <TableHeaderColumn dataField='PBS' csvHeader="PBS">PBS</TableHeaderColumn>
       <TableHeaderColumn dataField='GFT' csvHeader="GFT">GFT</TableHeaderColumn>
       <TableHeaderColumn dataField='MCS' csvHeader="MCS">MCS</TableHeaderColumn>
-        <TableHeaderColumn dataField='total' csvHeader="Total">Total</TableHeaderColumn>
+      <TableHeaderColumn dataField='total' csvHeader="Total">Total</TableHeaderColumn>
+      
     </BootstrapTable>
   </div>
 );
--- client/src/containers/InventoryTotalPage.jsx
+++ client/src/containers/InventoryTotalPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 import Auth from '../modules/Auth';
 import InventoryTotal from '../components/InventoryTotal.jsx';
-
 
 class InventoryTotalPage extends React.Component {
 
@@ -12,59 +12,7 @@ class InventoryTotalPage extends React.Component {
     super(props);
 
     this.state = {
-      inventoryTotal: [
-        {
-
-          "from": "Troop",
-          "TAL": 168,
-          "SMR": 180,
-          "LEM": 132,
-          "SB": 123,
-          "TM": 490,
-          "PBP": 276,
-          "CD": 259,
-          "PBS": 120,
-          "GFT": 12,
-          "MCS": 23,
-        }, {
-          "from": "Girl Scouts",
-          "TAL": 127,
-          "SMR": 123,
-          "LEM": 95,
-          "SB": 79,
-          "TM": 322,
-          "PBP": 187,
-          "CD": 166,
-          "PBS": 55,
-          "GFT": 8,
-          "MCS": 21,
-        }, {
-          "from": "Booths",
-          "TAL": 27,
-          "SMR": 157,
-          "LEM": 37,
-          "SB": 44,
-          "TM": 168,
-          "PBP": 83,
-          "CD": 93,
-          "PBS": 61,
-          "GFT": 4,
-          "MCS": 2,
-        },
-        {
-          "from": "Remaining",
-          "TAL": 27,
-          "SMR": 157,
-          "LEM": 37,
-          "SB": 44,
-          "TM": 168,
-          "PBP": 83,
-          "CD": 93,
-          "PBS": 61,
-          "GFT": 4,
-          "MCS": 2,
-        }
-      ]
+      inventoryTotal: []
     };
   }
 
@@ -73,24 +21,147 @@ class InventoryTotalPage extends React.Component {
    */
   componentDidMount() {
     console.log("InventoryTotalPage: componentDidMount");
-    const xhr = new XMLHttpRequest();
-    xhr.open('get', '/api/inventoryTotal');
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    // set the authorization HTTP header
-    xhr.setRequestHeader('Authorization', `bearer ${Auth.getToken()}`);
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', () => {
-      if (xhr.status === 200) {
-        console.log("InventoryTotalPage: status 200");
-        this.setState({
-          inventoryTotal: xhr.response.inventoryTotal
-        });
-      }
-      else {
-        console.log("Inventory Total Page: status: " + xhr.status);
-      }
-    });
-    xhr.send();
+    let troopTotal = {};
+    let girlTotal = {};
+    let boothTotal = {};
+    let remaining = {};
+    let newInventoryTotal = [];
+
+    axios.get('/api/troopCookie', {headers: {'Authorization': `bearer ${Auth.getToken()}`}})
+        .then(function (response) {
+            if (response.data && response.data.troopCookies) {
+                troopTotal = this.addUpCookies("Troop", response.data.troopCookies);
+                newInventoryTotal.push(troopTotal);
+                
+                axios.get('/api/girlCookie', {headers: {'Authorization': `bearer ${Auth.getToken()}`}})
+                  .then(function (response) {
+                    if (response.data && response.data.girlCookies) {
+                      girlTotal = this.addUpCookies("Girl Scouts", response.data.girlCookies);
+                      newInventoryTotal.push(girlTotal);
+
+                      axios.get('/api/boothCookie', {headers: {'Authorization': `bearer ${Auth.getToken()}`}})
+                        .then(function (response) {
+                          if (response.data && response.data.boothCookies) {
+                            boothTotal = this.addUpCookies("Booths", response.data.boothCookies);
+                            newInventoryTotal.push(boothTotal);
+
+                            remaining = this.calculateRemaining(troopTotal, girlTotal, boothTotal);
+                            newInventoryTotal.push(remaining);
+
+                            this.setState({
+                              inventoryTotal: newInventoryTotal
+                            });
+                          }
+                        }.bind(this))
+                        .catch(function (error) {
+                          console.log(error);
+                        });
+                    }
+                  }.bind(this))
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+            }
+          }.bind(this))
+          .catch(function (error) {
+            console.log(error);
+          });
+  }               
+
+  addUpCookies(from, cookies) {
+    console.log("addUpCookies called");
+
+    var total = {
+      "from": from,
+      "TAL": 0,
+      "SMR": 0,
+      "LEM": 0,
+      "SB": 0,
+      "TM": 0,
+      "PBP": 0,
+      "CD": 0,
+      "PBS": 0,
+      "GFT": 0,
+      "MCS": 0,
+      "total": 0
+    }
+
+    total.from = from;
+
+    if(cookies.length === 0){
+        return
+    }
+
+    for(let i = 0; i < cookies.length; i++){
+        total.TAL += cookies[i].TAL;
+        total.SMR += cookies[i].SMR;
+        total.LEM += cookies[i].LEM;
+        total.SB += cookies[i].SB;
+        total.TM += cookies[i].TM;
+        total.PBP += cookies[i].PBP;
+        total.CD += cookies[i].CD;
+        total.PBS += cookies[i].PBS;
+        total.GFT += cookies[i].GFT;
+        total.MCS += cookies[i].MCS;
+    }
+
+    total.total = 
+    total.TAL +
+    total.SMR + 
+    total.LEM + 
+    total.SB + 
+    total.TM + 
+    total.PBP + 
+    total.CD + 
+    total.PBS + 
+    total.GFT + 
+    total.MCS; 
+
+    return total;
+  }
+
+  calculateRemaining(troop, girl, booth) {
+    console.log("calculateRemaining called");
+    
+    var remaining = {
+      "from": "Remaining",
+      "TAL": 0,
+      "SMR": 0,
+      "LEM": 0,
+      "SB": 0,
+      "TM": 0,
+      "PBP": 0,
+      "CD": 0,
+      "PBS": 0,
+      "GFT": 0,
+      "MCS": 0,
+      "total": 0
+    }
+
+    remaining.TAL = troop.TAL - girl.TAL - booth.TAL;
+    remaining.SMR = troop.SMR- girl.SMR - booth.SMR;
+    remaining.LEM = troop.LEM - girl.LEM - booth.LEM;
+    remaining.SB = troop.SB - girl.SB - booth.SB;
+    remaining.TM = troop.TM - girl.TM - booth.TM;
+    remaining.PBP = troop.PBP - girl.PBP - booth.PBP;
+    remaining.CD = troop.CD - girl.CD - booth.CD;
+    remaining.PBS = troop.PBS - girl.PBS - booth.PBS;
+    remaining.GFT = troop.GFT - girl.GFT - booth.GFT;
+    remaining.MCS = troop.MCS - girl.MCS - booth.MCS;
+
+    remaining.total = 
+    remaining.TAL +
+    remaining.SMR + 
+    remaining.LEM + 
+    remaining.SB + 
+    remaining.TM + 
+    remaining.PBP + 
+    remaining.CD + 
+    remaining.PBS + 
+    remaining.GFT + 
+    remaining.MCS;
+
+    return remaining;
   }
 
   /**
